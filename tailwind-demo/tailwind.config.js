05c6a9f0a5dbc3250d7dbb357ed070380eddb52d
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-10-13 14:34:07 -0700

    files added for tailwind demo

---
+++ tailwind-demo/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  module.exports = {
+
+    plugins: [
+        require('flowbite/plugin')
+    ]
+
+}
+module.exports = {
+
+    content: [
+        "./node_modules/flowbite/**/*.js"
+    ]
+
+}
